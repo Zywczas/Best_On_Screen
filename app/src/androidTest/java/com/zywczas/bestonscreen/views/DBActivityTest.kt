f4ff32ece13c949e25f724d31a2657d6ba8c235e
Zywczas <zywczas@o2.pl>
2020-07-30 09:09:40 +0200

    first test class created

---
+++ app/src/androidTest/java/com/zywczas/bestonscreen/views/DBActivityTest.kt
@@ -0,0 +1,13 @@
+package com.zywczas.bestonscreen.views
+
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4ClassRunner::class)
+class DBActivityTest {
+    @Test
+    fun test_isActivityInView() {
+        TODO("Not yet implemented")
+    }
+}
