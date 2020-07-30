6511103d459979fae040dfaea1f3e88b85abe8ce
Zywczas <zywczas@o2.pl>
2020-07-30 12:38:26 +0200

    pierwszy test napisany

--- app/src/androidTest/java/com/zywczas/bestonscreen/views/DBActivityTest.kt
+++ app/src/androidTest/java/com/zywczas/bestonscreen/views/DBActivityTest.kt
@@ -1,6 +1,12 @@
 package com.zywczas.bestonscreen.views
 
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import com.zywczas.bestonscreen.R
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -8,6 +14,10 @@ import org.junit.runner.RunWith
 class DBActivityTest {
     @Test
     fun test_isActivityInView() {
-        TODO("Not yet implemented")
+        //testuje czy activity sie wlacza
+        val activityScenario = ActivityScenario.launch(DBActivity::class.java)
+
+        //czy layout jest pokazywany
+        onView(withId(R.id.drawer_layout)).check(matches(isDisplayed()))
     }
 }
