2d1ae24279dc648b0aa2529ba6aef5d6782c1594
Zywczas <zywczas@o2.pl>
2020-09-16 17:30:07 +0200

    MovieDaoTest.kt - first test written

--- app/src/androidTest/java/com/zywczas/bestonscreen/model/db/MovieDaoTest.kt
+++ app/src/androidTest/java/com/zywczas/bestonscreen/model/db/MovieDaoTest.kt
@@ -1,17 +1,28 @@
 package com.zywczas.bestonscreen.model.db
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.zywczas.bestonscreen.util.TestUtil
+import org.junit.Assert.*
 import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
 
 internal class MovieDaoTest : MoviesDataBaseTest() {
 
-    @Rule
+    //it has to have @JvmField annotation to 'make this rule public' - otherwise gives an error in Kotlin
+    @Rule @JvmField
     val rule = InstantTaskExecutorRule()
 
     @Test
-    fun test1(){
-
+    fun insert(){
+        //arrange
+        val movieFromDb = TestUtil.movieFromDB1
+        //act
+        val rowId = dao.insertMovie(movieFromDb).blockingGet()
+        val movieIdCount = dao.getIdCount(movieFromDb.id).blockingFirst()
+        //assert
+        assertEquals(1, movieIdCount)
     }
 
 }
--- app/src/androidTest/java/com/zywczas/bestonscreen/model/db/MoviesDataBaseTest.kt
+++ app/src/androidTest/java/com/zywczas/bestonscreen/model/db/MoviesDataBaseTest.kt
@@ -4,14 +4,13 @@ import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import org.junit.After
 import org.junit.Before
-import org.junit.jupiter.api.Assertions.*
 
 internal abstract class MoviesDataBaseTest {
 
     //system under test
-    lateinit var dataBase: MoviesDataBase
+    private lateinit var dataBase: MoviesDataBase
     lateinit var dao: MovieDao
-    //todo spawdzic czy nie dac private
+
     @Before
     fun init(){
         dataBase = Room.inMemoryDatabaseBuilder(
