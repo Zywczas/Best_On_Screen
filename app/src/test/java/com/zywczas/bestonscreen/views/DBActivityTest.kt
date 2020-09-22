9fe08c2a1383d7607e0ee6de94c4de9ca00cb3b4
Zywczas <zywczas@o2.pl>
2020-09-22 13:42:59 +0200

    cleaned comments in DBActivityTest.kt

--- app/src/test/java/com/zywczas/bestonscreen/views/DBActivityTest.kt
+++ app/src/test/java/com/zywczas/bestonscreen/views/DBActivityTest.kt
@@ -1,13 +1,13 @@
 package com.zywczas.bestonscreen.views
 
-import androidx.lifecycle.Lifecycle
-import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.zywczas.bestonscreen.R
 import org.hamcrest.CoreMatchers.not
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.annotation.LooperMode
@@ -16,24 +16,29 @@ import org.robolectric.annotation.LooperMode
 @LooperMode(LooperMode.Mode.PAUSED)
 internal class DBActivityTest {
 
+    @Rule
+    @JvmField
+    val activityRule = ActivityScenarioRule(DBActivity::class.java)
+
     @Test
-    fun isActivityInView(){
-        val scenario = ActivityScenario.launch(DBActivity::class.java)
-        scenario.moveToState(Lifecycle.State.RESUMED)
+    fun isActivityInView() {
         onView(withId(R.id.drawer_layout))
             .check(matches(isDisplayed()))
     }
 
     @Test
-    fun visibility_toolbar_RecyclerView_progressbar_textView(){
-        val scenario = ActivityScenario.launch(DBActivity::class.java)
-        scenario.moveToState(Lifecycle.State.RESUMED)
+    fun visibility_toolbar_RecyclerView_progressbar_textView() {
         onView(withId(R.id.toolbar)).check(matches(isDisplayed()))
         onView(withId(R.id.moviesRecyclerView)).check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
         onView(withId(R.id.progressBar)).check(matches(not(isDisplayed())))
         onView(withId(R.id.emptyListTextView)).check(matches(isDisplayed()))
     }
 
+    @Test
+    fun isContentDisplayed() {
+        onView(withId(R.id.toolbar)).check(matches(hasDescendant(withText("Movies: My List"))))
 
+    }
 
 }
+
