b53bcf2835919c1df62e4a191c100b95b4e88b74
Zywczas <zywczas@o2.pl>
2020-09-23 10:39:40 +0200

    orientation change sorted

--- app/src/main/java/com/zywczas/bestonscreen/views/DBFragment.kt
+++ app/src/main/java/com/zywczas/bestonscreen/views/DBFragment.kt
@@ -1,8 +1,10 @@
 package com.zywczas.bestonscreen.views
 
+import android.content.Context
 import android.content.Intent
 import android.content.res.Configuration
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -31,6 +33,11 @@ class DBFragment constructor(
 
     private val viewModel : DBVM by viewModels { viewModelFactory }
     private lateinit var adapter: MovieAdapter
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        Log.d("film", "on attach - fragment stworzony od nowa")
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
--- app/src/main/java/com/zywczas/bestonscreen/views/MainActivity.kt
+++ app/src/main/java/com/zywczas/bestonscreen/views/MainActivity.kt
@@ -2,6 +2,7 @@ package com.zywczas.bestonscreen.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import com.zywczas.bestonscreen.App
 import com.zywczas.bestonscreen.R
 import javax.inject.Inject
@@ -12,20 +13,29 @@ class MainActivity : AppCompatActivity() {
     lateinit var movieFragmentFactory: MovieFragmentFactory
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        //todo to musi byc przed onCreate
+        App.moviesComponent.inject(this)
+        supportFragmentManager.fragmentFactory = movieFragmentFactory
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //todo poukladac to w lambdy zaleznosci
-        App.moviesComponent.inject(this)
-        startDBFragment()
+            startDBFragment()
+
     }
 
     private fun startDBFragment(){
         //todo supportFragmentManager jest dla wszystkich fragmentow w tym activity wiec jak go zmienie w trakcie to nie wiem czy sie nie pomiesza wszystko
         //todo ale fragment factory chyba mozna zmieniac, wiec powinno byc ok
         //todo mozliwe ze to powinno byc przed seper.onCreate
-        supportFragmentManager.fragmentFactory = movieFragmentFactory
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.fragmentContainer, DBFragment::class.java, null)
-            .commit()
+
+        if (supportFragmentManager.fragments.size == 0){
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainer, DBFragment::class.java, null)
+                .commit()
+        }
+
+
     }
+
+
 }
